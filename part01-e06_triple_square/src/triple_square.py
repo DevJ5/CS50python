d3ce0c3fbb5bf8c529e50c702b23ced13f3ae057
DevJ5 <idevelopjs@gmail.com>
2024-05-11 15:33:59 +0200

    Exercise 4-6 done

--- part01-e06_triple_square/src/triple_square.py
+++ part01-e06_triple_square/src/triple_square.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
+def triple(n):
+    return n * 3
+
+
+def square(n):
+    return n**2
 
 
 def main():
-    pass
+
+    for i in range(1, 11):
+        iTripled = triple(i)
+        iSquared = square(i)
+        if iSquared > iTripled:
+            break
+        print(f"triple({i})=={iTripled}", f"square({i})=={iSquared}")
+
 
 if __name__ == "__main__":
     main()
