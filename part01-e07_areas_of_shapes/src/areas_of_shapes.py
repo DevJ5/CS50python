679a41536f404f37e24bab09c6bef1695ebc7b8c
DevJ5 <idevelopjs@gmail.com>
2024-05-12 07:40:06 +0200

    Exercise 7-8 done

--- part01-e07_areas_of_shapes/src/areas_of_shapes.py
+++ part01-e07_areas_of_shapes/src/areas_of_shapes.py
@@ -4,7 +4,35 @@ import math
 
 
 def main():
-    # enter you solution here
+    # Area of triangle, rectangle, circle
+    SHAPE_TRIANGLE = "triangle"
+    SHAPE_RECTANGLE = "rectangle"
+    SHAPE_CIRCLE = "circle"
+
+    area = 0
+
+    while True:
+        iShape = input(
+            f"Choose a shape ({SHAPE_TRIANGLE}, {SHAPE_RECTANGLE}, {SHAPE_CIRCLE}): "
+        )
+        if iShape == "":
+            break
+        if iShape == SHAPE_TRIANGLE:
+            iBase = int(input("Give base of the triangle: "))
+            iHeight = int(input("Give height of the triangle: "))
+            area = iBase * iHeight / 2
+        elif iShape == SHAPE_RECTANGLE:
+            iBase = int(input("Give base of the triangle: "))
+            iHeight = int(input("Give height of the triangle: "))
+            area = iBase * iHeight
+        elif iShape == SHAPE_CIRCLE:
+            iRadius = int(input("Give radius of the circle: "))
+            area = iRadius**2 * math.pi
+        else:
+            print("Unknown shape!")
+            continue
+        print(f"The area is {area:.4f}")
+
 
 if __name__ == "__main__":
     main()
--- part01-e08_solve_quadratic/src/solve_quadratic.py
+++ part01-e08_solve_quadratic/src/solve_quadratic.py
@@ -2,12 +2,25 @@
 
 import math
 
+
 def solve_quadratic(a, b, c):
-    return (0,0)
+    # ax^2 + bx + c
+    discriminant = math.sqrt(b**2 - 4 * a * c)
+    if discriminant < 0:
+        print("Complex number")
+    elif discriminant == 0:
+        root1 = -b / (2 * a)
+        return (root1, root1)
+    else:
+        root1 = (-b + discriminant) / (2 * a)
+        root2 = (-b - discriminant) / (2 * a)
+        return (root1, root2)
 
 
 def main():
-    pass
+    print(solve_quadratic(1, -3, 2))
+    print(solve_quadratic(1, 2, 1))
+
 
 if __name__ == "__main__":
     main()
