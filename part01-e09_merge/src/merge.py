c5fd77092af9b1ff2f91362ab6953a866e088f67
DevJ5 <idevelopjs@gmail.com>
2024-05-12 09:29:59 +0200

    Exercise 9-10 done

--- part01-e09_merge/src/merge.py
+++ part01-e09_merge/src/merge.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python3
 
+
 def merge(L1, L2):
-    return []
+    sizeL1 = len(L1)
+    sizeL2 = len(L2)
+
+    i = j = 0
+    mergedList = []
+
+    while i < sizeL1 and j < sizeL2:
+        if L1[i] <= L2[j]:
+            mergedList.append(L1[i])
+            i += 1
+        else:
+            mergedList.append(L2[j])
+            j += 1
+
+    mergedList = mergedList + L1[i:] + L2[j:]
+    return mergedList
+
 
 def main():
-    pass
+    # Two lists L1 and L2 with ints sorted asc
+    L1 = [1, 5, 9, 12]
+    L2 = [2, 6, 10]
+    mergedList = merge(L1, L2)
+    print(mergedList)
+
 
 if __name__ == "__main__":
     main()
--- part01-e10_detect_ranges/src/detect_ranges.py
+++ part01-e10_detect_ranges/src/detect_ranges.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
+
 def detect_ranges(L):
-    return []
+    sortedL = sorted(L)
+    result = []
+    range = None
+
+    print(sortedL)
+    for i, number in enumerate(sortedL):
+        if i + 1 < len(sortedL) and number + 1 == sortedL[i + 1]:
+            if range == None:
+                range = (number, sortedL[i + 1] + 1)
+            else:
+                range = (range[0], sortedL[i + 1] + 1)
+        else:
+            if range == None:
+                range = number
+            result.append(range)
+            range = None
+
+    return result
+
 
 def main():
     L = [2, 5, 4, 8, 12, 6, 7, 10, 13]
     result = detect_ranges(L)
-    print(L)
     print(result)
+
 
 if __name__ == "__main__":
     main()
