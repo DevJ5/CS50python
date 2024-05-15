a6c2c1ddb437abcb162eaa52cdf4714767ab9012
DevJ5 <idevelopjs@gmail.com>
2024-05-15 11:03:28 +0200

    Exercise 12 done

--- part01-e12_distinct_characters/src/distinct_characters.py
+++ part01-e12_distinct_characters/src/distinct_characters.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
+
 def distinct_characters(L):
-    return {}
+    result = {}
+    for str in L:
+        result[str] = len(set(str))
+
+    return result
+
 
 def main():
     print(distinct_characters(["check", "look", "try", "pop"]))
+
 
 if __name__ == "__main__":
     main()
