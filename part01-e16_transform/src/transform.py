8534a2e581558c2306f6676f7fc71ea038818a22
DevJ5 <idevelopjs@gmail.com>
2024-05-15 15:59:14 +0200

    Exercise 16 done

--- part01-e16_transform/src/transform.py
+++ part01-e16_transform/src/transform.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
+
 def transform(s1, s2):
-    return []
+    # result = []
+    # s1Ints = list(map(int, s1.split()))
+    # s2Ints = list(map(int, s2.split()))
+
+    # zippedList = list(zip(s1Ints, s2Ints))
+    # result = list(map(lambda x: x[0] * x[1], zippedList))
+
+    # return result
+
+    return [x * y for (x, y) in zip(map(int, s1.split()), map(int, s2.split()))]
+
 
 def main():
-    pass
+    print(transform("1 5 3", "2 6 -1"))
+
 
 if __name__ == "__main__":
     main()
