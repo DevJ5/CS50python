a106810eb1f761a355fa2895a4d0509979068f22
DevJ5 <idevelopjs@gmail.com>
2024-05-15 08:57:51 +0200

    Exercises 11 done

--- part01-e11_interleave/src/interleave.py
+++ part01-e11_interleave/src/interleave.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
+
 def interleave(*lists):
-    return []
+    zippedLists = list(zip(*lists))
+
+    # Faster way
+    return list(sum(zippedLists, ()))
+
+    # result = []
+    # for l in zippedLists:
+    #     for i in l:
+    #         result.extend([i])
+    # return result
+
 
 def main():
-    print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
+    print(interleave([1, 2, 3], [20, 30, 40], ["a", "b", "c"]))
+
 
 if __name__ == "__main__":
     main()
