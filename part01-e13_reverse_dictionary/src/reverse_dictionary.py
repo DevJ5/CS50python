19ae3cd0a9ae49d170aa480ee9f1370ae6d51f70
DevJ5 <idevelopjs@gmail.com>
2024-05-15 11:25:21 +0200

    Exercise 13 done

--- part01-e13_reverse_dictionary/src/reverse_dictionary.py
+++ part01-e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 
+
 def reverse_dictionary(d):
-    return {}
+    result = {}
+
+    for key, value in d.items():
+        for v in value:
+            try:
+                result[v].append(key)
+            except:
+                result[v] = [key]
+
+    return result
+
 
 def main():
-    pass
+    d = {
+        "move": ["liikuttaa"],
+        "hide": ["piilottaa", "salata"],
+        "six": ["kuusi"],
+        "fir": ["kuusi"],
+    }
+
+    print(reverse_dictionary(d))
+
 
 if __name__ == "__main__":
     main()
