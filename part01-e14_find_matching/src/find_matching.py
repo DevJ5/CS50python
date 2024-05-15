9b2d71f362d59be86564a563d39bf1fd3f5c0c32
DevJ5 <idevelopjs@gmail.com>
2024-05-15 11:56:40 +0200

    Exercise 14 done

--- part01-e14_find_matching/src/find_matching.py
+++ part01-e14_find_matching/src/find_matching.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
+
 def find_matching(L, pattern):
-    return []
+    # L = List of strings
+    # pattern = search string
+
+    result = []
+    for i, str in enumerate(L):
+        if pattern in str:
+            result.append(i)
+
+    return result
+
 
 def main():
-    pass
+    find_matching(["sensitive", "engine", "rubbish", "comment"], "en")
+
 
 if __name__ == "__main__":
     main()
