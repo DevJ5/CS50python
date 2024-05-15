f08e57be3da210670e9a1a72a515031dd41e8edb
DevJ5 <idevelopjs@gmail.com>
2024-05-15 16:04:15 +0200

    Exercise 17 done

--- part01-e17_positive_list/src/positive_list.py
+++ part01-e17_positive_list/src/positive_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
+
 def positive_list(L):
-    return []
+    positiveNumbers = list(filter(lambda x: x > 0, L))
+    return positiveNumbers
+
 
 def main():
-    pass
+    positive_list([2, -2, 0, 1, -7])
+
 
 if __name__ == "__main__":
     main()
