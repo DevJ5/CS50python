ab7cfd81ccce67ef2e178fd4e134773a5a9a0c3e
DevJ5 <idevelopjs@gmail.com>
2024-05-16 07:08:33 +0200

    Exercise 19 done

--- part01-e19_sum_equation/src/sum_equation.py
+++ part01-e19_sum_equation/src/sum_equation.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
+from functools import reduce
+
 
 def sum_equation(L):
-    return ""
+
+    if len(L) == 0:
+        result = "0 = 0"
+    else:
+        sumL = reduce(lambda x, y: x + y, L, 0)
+        result = (" + ").join([str(x) for x in L])
+        result = (" = ").join([result, str(sumL)])
+
+    return result
+
 
 def main():
-    pass
+    print(sum_equation([]))
+
 
 if __name__ == "__main__":
     main()
