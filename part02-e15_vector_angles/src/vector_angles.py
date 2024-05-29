5fe386e9efd75cbf5a5d8b5fe8e5d2e334672924
DevJ5 <idevelopjs@gmail.com>
2024-05-29 14:25:04 +0200

    Remove ignored files

--- part02-e15_vector_angles/src/vector_angles.py
+++ part02-e15_vector_angles/src/vector_angles.py
@@ -5,16 +5,29 @@ import scipy.linalg
 
 
 def vector_angles(X, Y):
-    dotproduct = np.sum(X * Y)
-    magX = np.sqrt(np.sum(X**2))
-    magY = np.sqrt(np.sum(Y**2))
-    return dotproduct / (magX * magY)
+    result = np.zeros(X.shape[0])
+
+    for i in range(X.shape[0]):
+        dotproduct = np.sum(X[i] * Y[i])
+        magX = np.sqrt(np.sum(X[i] ** 2))
+        magY = np.sqrt(np.sum(Y[i] ** 2))
+        result[i] = dotproduct / (magX * magY)
+
+        # Alternative
+        # result[i] = (np.dot(X[i], Y[i])/scipy.linalg.norm(X[i])/scipy.linalg.norm(Y[i]))
+
+    result = np.clip(result, -1, 1)
+    return np.degrees(np.arccos(result))
 
 
 def main():
-    x = np.array([1, 2, 3])
-    y = np.array([4, 5, 6])
-    z = np.array([x, y])
+    # A vector has m dimensions
+    # An array has n,m shape, which is a 2d array
+    # Each row in the 2d array, which is n, is a vector of m dimension
+
+    # 1 Row example
+    x = np.array([[1, 2, 3]])
+    y = np.array([[4, 5, 6]])
 
     print(vector_angles(x, y))
 
--- part03-e02_first_half_second_half/src/first_half_second_half.py
+++ part03-e02_first_half_second_half/src/first_half_second_half.py
@@ -2,11 +2,21 @@
 
 import numpy as np
 
+
+# two dimensional array of shape (n,2*m) as parameter a
 def first_half_second_half(a):
-    return np.array([])
+    column_count = a.shape[1]
+    # Integer division otherwise slice doesn't work
+    m = column_count // 2
+    # sum of the first m elements larger than the sum of the last m elements on the row
+    b = np.sum(a[:, 0:m], axis=1) > np.sum(a[:, m:], axis=1)
+    return a[b]
+
 
 def main():
-    pass
+    a = np.array([[1, 3, 4, 2], [2, 2, 1, 2]])
+    first_half_second_half(a)
+
 
 if __name__ == "__main__":
     main()
