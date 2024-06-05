63e3e34a1edca4d41a7a78807e7244efebfe7484
DevJ5 <idevelopjs@gmail.com>
2024-06-05 11:16:50 +0200

    Part 03: Exercise 04 done

--- part03-e04_matrix_power/src/matrix_power.py
+++ part03-e04_matrix_power/src/matrix_power.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
+from functools import reduce
+import numpy as np
 
 
-def matrix_power(a, n):
-    np.eye(2)
+def matrix_power(m, n):
+    if n == 0:
+        return np.eye(m.shape[0])
+    elif n < 0:
+        m = np.linalg.inv(m)
+        n = -1 * n
+
+    genM = (m for i in range(n))
+    return reduce(lambda x, y: x @ y, genM)
+
 
 def main():
-    return
+    print(matrix_power(np.array([[1, 2], [3, 4]]), 0))
+
 
 if __name__ == "__main__":
     main()
