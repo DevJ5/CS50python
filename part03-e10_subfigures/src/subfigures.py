dc6fbef6679921346f7923e48d3f81760023e337
DevJ5 <idevelopjs@gmail.com>
2024-06-05 15:12:02 +0200

    Part 03: Exercise 09 and 10 done

--- part03-e10_subfigures/src/subfigures.py
+++ part03-e10_subfigures/src/subfigures.py
@@ -3,11 +3,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def subfigures(a):
-    pass
+
+def subfigures(m):
+    fig, ax = plt.subplots(1, 2)
+    print(ax.shape)
+    ax[0].plot(m[:, 0], m[:, 1])
+    ax[1].scatter(m[:, 0], m[:, 1], c=m[:, 2], cmap="viridis", s=m[:, 3])
+    plt.show()
+
 
 def main():
-    pass
+    a = np.array([[1, 1, 10, 20], [2, 2, 20, 20], [3, 3, 30, 40], [4, 4, 40, 40]])
+    subfigures(a)
+
 
 if __name__ == "__main__":
     main()
