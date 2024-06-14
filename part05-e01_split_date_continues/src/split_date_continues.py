f1ffc6600e6090deabb7eb173718df8654525329
DevJ5 <idevelopjs@gmail.com>
2024-06-14 15:20:34 +0200

    Part 05: Exercise 01 done

--- part05-e01_split_date_continues/src/split_date_continues.py
+++ part05-e01_split_date_continues/src/split_date_continues.py
@@ -1,13 +1,68 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import numpy as np
 
 
 def split_date(df):
+    df = df["Päivämäärä"].str.split(expand=True)
+    df.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
+    # Slice the first two digits of hourstring
+    df["Hour"] = df["Hour"].str[0:2]
+    # Take only the columns we need
+    df = df[["Weekday", "Day", "Month", "Year", "Hour"]]
+
+    # Dictionary for weekdays
+    translations_weekday = {
+        "ma": "Mon",
+        "ti": "Tue",
+        "ke": "Wed",
+        "to": "Thu",
+        "pe": "Fri",
+        "la": "Sat",
+        "su": "Sun",
+    }
+
+    # Dictionary for months
+    translations_months = {
+        "tammi": 1,
+        "helmi": 2,
+        "maalis": 3,
+        "huhti": 4,
+        "touko": 5,
+        "kesä": 6,
+        "heinä": 7,
+        "elo": 8,
+        "syys": 9,
+        "loka": 10,
+        "marras": 11,
+        "joulu": 12,
+    }
+    # Map the translations
+    df["Weekday"] = df["Weekday"].map(translations_weekday)
+    # Any value that isnt found will be an empty string, convert it to None
+    df["Weekday"] = df["Weekday"].replace("", np.nan)
+    df["Month"] = df["Month"].map(translations_months)
+    df["Month"] = df["Month"].replace("", np.nan)
+    # Convert all the columns except the first one (Weekday) to integers
+    df[df.columns[1:]] = df[df.columns[1:]].astype("int64")
+
     return df
 
+
 def split_date_continues():
-    return None
+    df = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";")
+    df = df.dropna(how="all")
+    df = df.dropna(axis=1, how="all")
+
+    first_col = df.iloc[:, [0]]
+    df_first_col = split_date(first_col)
+
+    df = df.drop(df.columns[0], axis=1)
+    df = pd.concat([df_first_col, df], axis=1)
+
+    return df
+
 
 def main():
     df = split_date_continues()
