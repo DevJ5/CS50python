d465b6deeaa69423d30fc0b290029375aee017dc
DevJ5 <idevelopjs@gmail.com>
2024-06-18 09:45:34 +0200

    Part 05: Exercise 12 & 13 done

--- part05-e12_coefficient_of_determination/src/coefficient_of_determination.py
+++ part05-e12_coefficient_of_determination/src/coefficient_of_determination.py
@@ -5,9 +5,29 @@ from sklearn import linear_model
 
 
 def coefficient_of_determination():
-    return []
-    
+    df = pd.read_csv('src/mystery_data.tsv', sep='\t')
+
+    X = df.loc[:, 'X1':'X5']
+    y = df.loc[:, 'Y']
+
+    model = linear_model.LinearRegression(fit_intercept=True)
+
+    model.fit(X, y)
+    score = model.score(X, y)
+
+    r2_scores = [score]
+    for i in range(len(X.columns)):
+        Xi = X.iloc[:, i].values.reshape(-1, 1)
+        model.fit(Xi, y)
+        r2_scores.append(model.score(Xi, y))
+
+    return r2_scores
+
 def main():
+    r2_scores = coefficient_of_determination()
+    print(f"R2-score with feature(s) X: {r2_scores[0]}")
+    for i in range(1, len(r2_scores)):
+        print(f"R2-score with feature(s) X{i}: {r2_scores[i]}")
 
 if __name__ == "__main__":
     main()
--- part05-e13_cycling_weather_continues/src/cycling_weather_continues.py
+++ part05-e13_cycling_weather_continues/src/cycling_weather_continues.py
@@ -1,12 +1,86 @@
 #!/usr/bin/env python3
 
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+def read_weather_data():
+    df = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";")
+    df = df.dropna(how="all")
+    df = df.dropna(axis=1, how="all")
+    split_column = df["Päivämäärä"].str.split("\s", expand=True)
+    df[["Weekday", "Day", "Month", "Year", "Hour"]] = split_column
+    df["Hour"] = df["Hour"].str[0:2]
+    df.drop("Päivämäärä", axis=1, inplace=True)
+    new_column_order = ["Weekday", "Day", "Month", "Year", "Hour"] + [
+        col
+        for col in df.columns
+        if col not in ["Weekday", "Day", "Month", "Year", "Hour"]
+    ]
+    df = df[new_column_order]
+
+    weekdays = {
+        "ma": "Mon",
+        "ti": "Tue",
+        "ke": "Wed",
+        "to": "Thu",
+        "pe": "Fri",
+        "la": "Sat",
+        "su": "Sun",
+    }
+
+    months = {
+        "tammi": 1,
+        "helmi": 2,
+        "maalis": 3,
+        "huhti": 4,
+        "touko": 5,
+        "kesä": 6,
+        "heinä": 7,
+        "elo": 8,
+        "syys": 9,
+        "loka": 10,
+        "marras": 11,
+        "joulu": 12,
+    }
+
+    df["Weekday"] = df["Weekday"].replace(weekdays)
+    df["Month"] = df["Month"].replace(months)
+    df = df.astype(
+        {"Weekday": object, "Day": int, "Month": int, "Year": int, "Hour": int}
+    )
+    return df
 
 
 def cycling_weather_continues(station):
-    return ((0.0, 0.0, 0.0), 0.0)
-    
+    df1 = read_weather_data()
+    selected_columns = df1.columns[:5].tolist() + [station]
+    df1 = df1[selected_columns]
+    df1 = df1[df1["Year"] == 2017]
+    df1 = df1.groupby(["Month", "Day"])[station].sum().reset_index()
+    df2 = pd.read_csv("src/kumpula-weather-2017.csv")
+    df = pd.merge(df1, df2, left_on=["Month", "Day"], right_on=["m", "d"])
+    df = df.drop(columns=["Year", "Time", "Time zone","m","d"])
+    df = df.ffill()
+    nan_columns = df.columns[df.isna().any()].tolist()
+    print(nan_columns)
+    # Splitting into X and y
+    X = df.iloc[:, -3:]  # Features
+    y = df[station]  # Target variable
+    model = LinearRegression(fit_intercept=True)
+    model.fit(X, y)
+    score = model.score(X, y)
+    return model.coef_, score
+
+
 def main():
-    return
+    station = "Baana"
+    coef, score = cycling_weather_continues(station)
+    print(f"Measuring station: {station}")
+    print(f"Regression coefficient for variable 'precipitation': {coef[0]:.1f}")
+    print(f"Regression coefficient for variable 'snow depth': {coef[1]:.1f}")
+    print(f"Regression coefficient for variable 'temperature': {coef[2]:.1f}")
+    print(f"Score: {score:.2f}")
 
 if __name__ == "__main__":
     main()
