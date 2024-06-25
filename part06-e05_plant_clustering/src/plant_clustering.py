218e57b72d9141da9b77f81d1537eb5f2488691a
DevJ5 <idevelopjs@gmail.com>
2024-06-25 12:44:27 +0200

    Part 06: Exercise 05 & 06 done

--- part06-e05_plant_clustering/src/plant_clustering.py
+++ part06-e05_plant_clustering/src/plant_clustering.py
@@ -1,22 +1,47 @@
 #!/usr/bin/env python3
 
 import scipy
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+import pandas as pd
 
 
 def find_permutation(n_clusters, real_labels, labels):
-    permutation=[]
+    permutation = []
     for i in range(n_clusters):
         idx = labels == i
         # Choose the most common label among data points in the cluster
-        new_label=scipy.stats.mode(real_labels[idx])[0][0]
+        new_label = scipy.stats.mode(real_labels[idx])[0]
         permutation.append(new_label)
     return permutation
 
+
 def plant_clustering():
-    return 0.0
+    iris = load_iris()
+    df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+    print(df.head())
+    # Initialize the KMeans object with 3 clusters (since we know there are 3 species)
+    model = KMeans(n_clusters=3, random_state=0)
+    # Fit the KMeans model to the data
+    model.fit(df)
+    # Print the cluster centers
+    print("Cluster centers:")
+    print(model.cluster_centers_)
+    # Print the cluster labels (assigned to each data point)
+    print("\nCluster labels:")
+    print(model.labels_)
+
+    permutation = find_permutation(3, iris.target, model.labels_)
+    acc = accuracy_score(iris.target, [permutation[label] for label in model.labels_])
+    print("Accuracy score is", acc)
+
+    return acc
+
 
 def main():
     print(plant_clustering())
+
 
 if __name__ == "__main__":
     main()
--- part06-e06_nonconvex_clusters/src/nonconvex_clusters.py
+++ part06-e06_nonconvex_clusters/src/nonconvex_clusters.py
@@ -4,13 +4,68 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import scipy
+
+
+def find_permutation(n_clusters, real_labels, labels):
+    permutation = []
+    for i in range(n_clusters):
+        idx = labels == i
+        # Choose the most common label among data points in the cluster
+        new_label = scipy.stats.mode(real_labels[idx])[0]
+        permutation.append(new_label)
+    return permutation
 
 
 def nonconvex_clusters():
-    return pd.DataFrame()
+    df = pd.read_csv("src/data.tsv", sep="\t")
+    # EDA
+    print(df.head())
+    print(df.shape)
+    print(df.info())
+    print(df.describe())
+    print("---------------------------- END EDA ----------------------------")
+
+    # Train model
+    X, y = df.iloc[:, :-1].values, df.iloc[:, -1].values
+    num_clusters_y = len(set(y))
+    result = []
+
+    for v in np.arange(0.05, 0.2, 0.05):
+        model = DBSCAN(eps=v)
+        model.fit(X)
+
+        # Count unique labels, ignoring noise (-1)
+        num_clusters = len(set(model.labels_)) - (1 if -1 in model.labels_ else 0)
+        num_noise = list(model.labels_).count(-1)
+
+        permutation = find_permutation(num_clusters, y, model.labels_)
+        new_labels = [
+            permutation[label] for label in model.labels_
+        ]  # permute the labels
+
+        if num_clusters_y != num_clusters:
+            acc_score = None
+        else:
+            noise_mask = model.labels_ == -1
+            acc_score = accuracy_score(y[~noise_mask], np.array(new_labels)[~noise_mask])
+
+        # plt.figure(figsize=(8, 6))
+        # plt.scatter(X[:, 0], X[:, 1], c=model.labels_)
+        # plt.show()
+
+        result.append([v, acc_score, num_clusters, num_noise])
+
+    df = pd.DataFrame(
+        result, columns=["eps", "Score", "Clusters", "Outliers"], dtype=float
+    )
+    return df
+
 
 def main():
     print(nonconvex_clusters())
+
 
 if __name__ == "__main__":
     main()
